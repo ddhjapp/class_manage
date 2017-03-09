3c6c39b7a2cd1d1f03782b60632346be37463eb5
Yangcl <794867067@qq.com>
2017-03-09 23:16:44 +0800

    注册完成

--- assets/purl.js
+++ assets/purl.js
@@ -5,6 +5,7 @@ var purl = {
 	url0001: path + 'edu/start_lesson.do',  // 教师开课接口
 	url0002: path + 'edu/sign_list.do',   // 已签到学生列表接口
 	url0003: path + 'edu/student_sign.do' ,   // 学生签到接口
-	url0004: path + 'edu/login.do'    // 用户登陆接口 
+	url0004: path + 'edu/login.do',    // 用户登陆接口
+	url0005: path + 'edu/registe.do'  // 用户注册接口 
 	
 }
